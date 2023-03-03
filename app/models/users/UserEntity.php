0ec7b41646dc1765ead5975b4cddb181504f8132
taha khorasani <khorasani@gmail.com>
2023-03-03 18:10:31 +0330

    add user entity

--- app/models/users/UserEntity.php
+++ app/models/users/UserEntity.php
@@ -1,5 +1,30 @@
 <?php
 namespace App\models\users;
 abstract class UserEntity{
-    $
+    protected int $id;
+    protected string $firstName;
+    protected string $lastName;
+    protected string $email;
+    protected string $password;
+    protected string $is_activated;
+
+    /**
+     * @param int $id
+     * @param string $firstName
+     * @param string $lastName
+     * @param string $email
+     * @param string $password
+     * @param string $is_activated
+     */
+    public function __construct(int $id, string $firstName, string $lastName, string $email, string $password, string $is_activated)
+    {
+        $this->id = $id;
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->email = $email;
+        $this->password = $password;
+        $this->is_activated = $is_activated;
+    }
+
+
 }
---
+++ app/models/users/UserModel.php
@@ -0,0 +1,13 @@
+<?php
+use App\models\users\UserEntity;
+use App\models\ModelClass;
+class UserModel extends UserEntity{
+    protected $ModelClass;
+
+
+    public function addUser()
+    {
+
+    }
+
+}
